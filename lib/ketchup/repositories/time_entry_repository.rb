3785e680a77b4a974fedeb8555cf5271cfb5ba96
lightalloy <lightallloy@gmail.com>
2017-04-16 09:25:01 +0300

    Order for stats

--- lib/ketchup/repositories/time_entry_repository.rb
+++ lib/ketchup/repositories/time_entry_repository.rb
@@ -2,7 +2,8 @@ class TimeEntryRepository < Hanami::Repository
 
   def for_user(user_id)
     return [] if user_id.to_s.empty?
-    time_entries.where(user_id: user_id)
+    # sql order desc wtf?
+    time_entries.where(user_id: user_id).order(:date).reverse
   end
 
   def one_for_user_by_id(user_id, id)
